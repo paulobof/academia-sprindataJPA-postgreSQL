80a1e4d4d23022cecb391d95006ed47ddd02da89
Paulo Bof <paulobof@gmail.com>
2022-08-20 16:34:17 -0300

    Criação APP RESTfull usando SPRINGDATA JPA + Postgre

---
+++ src/main/java/br/com/paulobof/academiaspringboot/AcademiaSpringbootApplication.java
@@ -0,0 +1,13 @@
+package br.com.paulobof.academiaspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademiaSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcademiaSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/paulobof/academiaspringboot/AcademiaSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.paulobof.academiaspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademiaSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
