1a216755f30770ce89abd286daf85ae03dbff512
Luan Fernandes <souluanf@icloud.com>
2022-08-21 14:33:20 -0300

    ajustes

--- src/main/java/br/com/paulobof/academiaspringboot/AcademiaSpringbootApplication.java
+++ src/main/java/br/com/paulobof/academiaspringboot/AcademiaSpringbootApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AcademiaSpringbootApplication{
-
     public static void main(String[] args) {
         SpringApplication.run(AcademiaSpringbootApplication.class, args);
     }
