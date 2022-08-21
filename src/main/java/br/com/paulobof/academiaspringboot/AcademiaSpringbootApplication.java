e31bebcbc001ec26c07209351806e6c4ba10eb94
Luan Fernandes <souluanf@icloud.com>
2022-08-21 14:32:08 -0300

    ajustes

--- src/main/java/br/com/paulobof/academiaspringboot/AcademiaSpringbootApplication.java
+++ src/main/java/br/com/paulobof/academiaspringboot/AcademiaSpringbootApplication.java
@@ -1,29 +1,14 @@
 package br.com.paulobof.academiaspringboot;
 
-import br.com.paulobof.academiaspringboot.repository.LogRepository;
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.List;
-
 @SpringBootApplication
-public class AcademiaSpringbootApplication implements CommandLineRunner {
-
-    private final LogRepository logRepository;
-
-    public AcademiaSpringbootApplication(LogRepository logRepository) {
-        this.logRepository = logRepository;
-    }
+public class AcademiaSpringbootApplication{
 
     public static void main(String[] args) {
         SpringApplication.run(AcademiaSpringbootApplication.class, args);
     }
 
-    @Override
-    public void run(String... args) {
-        logRepository.saveAll(List.of(
-                new Log()
-        ));
-    }
 }
